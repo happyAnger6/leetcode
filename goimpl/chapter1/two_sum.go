cba3abfe1803a3ad910497b02ae1938e38007cd3
happyAnger6 <happyAnger6@163.com>
2020-05-06 22:33:47 +0800

    chapter1 pass

--- goimpl/chapter1/two_sum.go
+++ goimpl/chapter1/two_sum.go
@@ -1,13 +1,11 @@
 package two_sum
 
-import "fmt"
-
 func TwoSum(nums []int, target int) []int {
 	imap := make(map[int]int)
 	for i, v := range nums {
 		need := target - v
-		if need_i, ok := imap[i]; ok {
-			return []int{i, need_i}
+		if need_i, ok := imap[need]; ok {
+			return []int{need_i, i}
 		}
 		imap[v] = i
 	}
--- goimpl/chapter1/two_sum_test.go
+++ goimpl/chapter1/two_sum_test.go
@@ -4,4 +4,36 @@ import (
 	"testing"
 )
 
-func TestTwoSum(
+func isEqual(a []int, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestTwoSum(t *testing.T) {
+	var tests = []struct {
+		results []int
+		input   []int
+		target  int
+	}{
+		{[]int{1, 2}, []int{3, 5, 4, 8, 1}, 9},
+		{[]int{1, 4}, []int{3, 5, 4, 8, 1}, 6},
+		{[]int{0, 2}, []int{3, 5, 4, 8, 1}, 7},
+		{nil, []int{3, 5, 4, 8, 1}, 88},
+	}
+
+	for _, test := range tests {
+		if got := TwoSum(test.input, test.target); !isEqual(got, test.results) {
+			t.Errorf("TwoSum(%v,%v) got:%v want:%v", test.input, test.target, got, test.results)
+		}
+	}
+
+}
